a8c3e6844b7c23aa1cabfcdc05a3163b63ea3d3a
AzureAD\ZaidAbu-Hijleh <zaida@windwardstudios.com>
2020-10-14 16:24:14 -0600

    Fixed run.ba tto work with MSBuild.

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
-msbuild RunReport.sln & bin\Debug\net472\RunReport.exe files\Manufacturing.docx files\output.docx -xml:MANF_DATA_2009 files\Manufacturing.xml & pause
+set "pathMSBuild=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin"
+set "execpath=%pathMSBuild%\MSBuild.exe"
+dotnet restore & "%execpath%" RunReport.sln & bin\Debug\net472\RunReport.exe files\Manufacturing.docx files\output.docx -xml:MANF_DATA_2009 files\Manufacturing.xml & pause
