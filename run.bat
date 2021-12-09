7decb3b2ed3fcecff8563f444e59ed73079da2ab
Zaid <zaida@windwardstudios.com>
2021-12-09 15:08:57 -0700

    fixed run.bat to work on any VS install type

--- run.bat
+++ run.bat
@@ -1,3 +1,10 @@
-set "pathMSBuild=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin"
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\" (
+set "pathMSBuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin")
+else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" (
+set "pathMSBuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin"
+)
+else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional" (
+set "pathMSBuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin"
+)
 set "execpath=%pathMSBuild%\MSBuild.exe"
 dotnet restore & "%execpath%" RunReport.sln & bin\Debug\net472\RunReport.exe files\Manufacturing.docx files\output.docx -xml:MANF_DATA_2009 files\Manufacturing.xml & pause
