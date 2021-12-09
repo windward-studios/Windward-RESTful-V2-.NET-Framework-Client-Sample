46d9db5ad3f050aa0761ddd9b7c1b10a0c2d1622
Zaid <zaida@windwardstudios.com>
2021-12-09 15:21:53 -0700

    fix bat file to use ifs

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\" (
 set "pathMSBuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin")
-else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" (
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" (
 set "pathMSBuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin"
 )
-else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional" (
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional" (
 set "pathMSBuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin"
 )
 set "execpath=%pathMSBuild%\MSBuild.exe"
